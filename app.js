0b5fbf627c1f8bb757ecfb331583f112b5cf8649
robinyeon <robin.yeon@gmail.com>
2021-12-10 20:27:07 +0900

    classlist

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    const currentColor = h1.style.color;
-    let newColor;
-    if (currentColor === "blue") {
-        newColor = "tomato";
+    const clickedClass = "clicked"
+    if (h1.classList.contains(clickedClass)) {
+        h1.classList.remove(clickedClass);
     } else {
-        newColor = "blue";
+        h1.classList.add(clickedClass);
     }
-    h1.style.color = newColor;
+    console.log(h1.className);
 }
 
 h1.addEventListener("click", handleTitleClick);
